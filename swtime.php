30e6173bebad1519dcdd186e4b6b9dc7bc0754e6
packetsdontlie <packetsdontlie@users.noreply.github.com>
2025-11-11 22:28:14 -0700

    Create swtime.php
    PHP version from Eric Kilfoil at ypass.net

---
+++ swtime.php
@@ -0,0 +1,11 @@
+/*
+ * note that PHP has built-in internet time functionality with the use of
+ * the date() command echo date("B"); will display the number of beats.  I
+ * find the MilliBeat to be more useful, so i use this code and format it
+ * appropriately with printf("%2.2f", swtime());
+ */
+
+function swtime()
+{
+  return((float) ((time()+3600) % 86400) / 86.4);
+}
